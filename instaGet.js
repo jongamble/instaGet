d01f88a50d68b837db5803c266fbcd069bd07dce
Jon Gamble <jon@royalflushdesigns.net>
2013-12-04 08:34:56 -0500

    return $.ajax instead of returning the data
    Allows for hooking into the ajax promise instead of just returning the data itself. Also cleaned up some of the logic so that ajax request was not in an each loop.

--- instaGet.js
+++ instaGet.js
@@ -1,7 +1,7 @@
 /*
  * instaGet
  * Created by: Jon Gamble - http://www.trimarkdigital.com
- * Version: 1.0
+ * Version: 1.1
  * Copyright: Trimark Digital
  * License: MIT license
  * Updated: November 13 2013
@@ -16,25 +16,30 @@
 			"key": null,
 			"access_token": null,
 			"count": 4,
-			"links": true
+			"links": true,
+			"liClass": ''
 		};
 		var user = $.extend(true, defaults, options);
-		return this.each(function () {
-			// Vars
-			var list = $(this);
-			var url = "https://api.instagram.com/v1/users/" + user.key + "/media/recent?access_token=" + user.access_token + "&count=" + user.count + "&callback=?";
+		var url = "https://api.instagram.com/v1/users/" + user.key + "/media/recent?access_token=" + user.access_token + "&count=" + user.count + "&callback=?";
+		var targets = this;
+
+		if(user.key == null || user.access_token == null){
+            targets.append("<li>You Forgot to put in your user_id and/or access_token!</li>");
+        }
 			
 			// Get the images	
-			$.ajax({
-				url: url,
-				dataType: 'json',
-				error: function(data, status, xhr){
-					console.log(data);
-				},
-				success: function(data){
-					if(data.data){
+		return $.ajax({
+			url: url,
+			dataType: 'json',
+			error: function(data, status, xhr){
+				console.log(data);
+			},
+			success: function(data){
+				if(data.data){
+					targets.each(function(){
+						var list = $(this);
 						$.each(data.data, function (i, val) {
-							var li = $("<li/>").appendTo(list);
+							var li = $("<li/>").addClass(user.liClass).appendTo(list);
 							if(user.links === true){
 								var a = $("<a/>", {"href": val.link, "target": "_blank"}).appendTo(li);
 								var img = $("<img/>", {"src": val.images.thumbnail.url}).appendTo(a);
@@ -46,16 +51,13 @@
 								a.attr("title", val.caption.text);
 							}
 						});
-					}else{
-						console.log(data.meta);
-					}
+					});
+				}else{
+					console.log(data.meta);
 				}
-			});
-			
-			if(user.key == null || user.access_token == null){
-			  list.append("<li>You Forgot to put in your user_id and/or access_token!</li>");
 			}
-
 		});
 	};
+		
+	
 })(jQuery);
