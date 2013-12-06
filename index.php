7d50f4302263f3ab318e9e8a0d1014a92b40abe9
Jon Gamble <jon@royalflushdesigns.net>
2013-12-06 10:54:43 -0500

    Return promise
    Refactoring for cleaner request. Rewriting the return to allow for
    hooking into the ajax promise instead of just returning content.

--- index.php
+++ index.php
@@ -3,17 +3,32 @@
 <head>
     <meta charset="utf-8">
     <title></title>
+	
+	<link href="bxslider.css" rel="stylesheet" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	
+	<script src="bxslider.js"></script>
 	<script src="instaGet.js"></script>
 	<script>
 		jQuery(document).ready(function($) {
-			$('.output-container').instaGet({
-				// get your user key and access token at http://projects.craftedpixelz.co.uk/jqinstapics/ - Bottom of the page
-				'key': '123456798', //Insert Key Here
-				'access_token': '1234565789.654984615.21654848.5165198498', //Insert Access Token Here
-				'count': '7', // How many pictures do you want to show up?
-				'links': true // Do you want the pictures to link to the original on instagram?
-			});
+			var instagramFeed = $('.output-container').instaGet({
+					// get your user key and access token at http://projects.craftedpixelz.co.uk/jqinstapics/ - Bottom of the page
+					'key': '32372829', // Insert user key here
+					'access_token': '32372829.674061d.1d2e3e104c394c7e8256685b4e5f6244',// Insert Access Token Here
+					'count': '7', // How many pictures do you want to show up?
+					'links': false // Do you want the pictures to link to the original on instagram?
+				}, function(){
+					instagramSlider = $('.output-container').bxSlider({
+						minSlides: 1,
+						maxSlides: 4,
+						slideWidth: 150,
+						slideMargin: 10,
+						moveSlides: 3
+						
+					});
+
+				}
+			);
 		});
 	</script> 
 </head>
